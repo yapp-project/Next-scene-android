a36c17a761e363697de9dcd401f7786b0b9369c1
하수경 <hasoo55@naver.com>
2019-10-10 22:54:06 +0900

    first commit

---
+++ app/src/main/java/com/android/yapp/scenetalker/PageFragment.java
@@ -0,0 +1,41 @@
+package com.android.yapp.scenetalker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PageFragment extends Fragment {
+
+    private int mPageNumber;
+    private ViewPagerIndicatorView indicatorView;
+
+    public static PageFragment create(int pageNumber) {
+        PageFragment fragment = new PageFragment();
+        Bundle args = new Bundle();
+        args.putInt("page", pageNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageNumber = getArguments().getInt("page");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_page, container, false);
+        ((TextView) rootView.findViewById(R.id.text)).setText(mPageNumber + "");
+
+       // indicatorView.setSelection(mPageNumber);
+
+        return rootView;
+    }
+
+
+}
